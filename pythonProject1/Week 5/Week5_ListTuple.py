c639f1b2d0d417298a397f65cb2f3673f861f242
Commit V1 <laviniaciobanescu94@yahoo.com>
2023-11-17 10:14:01 +0000

    Week 5

---
+++ pythonProject1/Week 5/Week5_ListTuple.py
@@ -0,0 +1,17 @@
+
+
+
+def directions():
+    steps =["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+    return steps
+
+def run_task1():
+    returned_steps = directions()
+    print(returned_steps)
+
+def list_tasks():
+    run_task1()
+
+list_tasks()
+
+
