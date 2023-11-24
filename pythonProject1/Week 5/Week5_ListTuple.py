ed1e9c1b0f654425fe3565cabc2db06e7485964c
Commit V1 <laviniaciobanescu94@yahoo.com>
2023-11-24 10:14:24 +0000

    get cwd directory

--- pythonProject1/Week 5/Week5_ListTuple.py
+++ pythonProject1/Week 5/Week5_ListTuple.py
@@ -30,12 +30,40 @@ def run_task2():
     for each_item in range(0, len(returned_path), 2):
         print(f"{returned_path[each_item]} for {returned_path[each_item+1]} steps")
 
-run_task2()
+def menu():
+    print("Please select a direction:")
+    returned_list = directions()
 
-def list_tasks():
-    run_task1()
+    for i in range(len(returned_list)):
+        print(f"{i}:{returned_list[i]}")
 
 
-list_tasks()
+def run_task3():
+    menu()
+
+def menu_and_input():
+    print("Please select a direction:")
+    returned_list = directions()
+
+    for i in range(len(returned_list)):
+        print(f"{i}:{returned_list[i]}")
+
+    direction = int(input())
+    return returned_list[direction]
+
+def run_task4():
+    route = []
+    print("Working out display route: ")
+
+    for each_time in range(5):
+        route.append(menu_and_input())
+
+    print(route)
+
+
+#run_task1()
+#run_task2()
+#run_task3()
+run_task4()
 
 
